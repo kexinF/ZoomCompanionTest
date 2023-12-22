4e2bd83606d49068ecfcaa9e27de3a09bafd650a
Ben Lickly <blickly@google.com>
2023-12-22 15:58:28 -0800

    Remove the 'use client' directive
    This doesn't actually turn off SSR

--- app/mindfulness/page.js
+++ app/mindfulness/page.js
@@ -1,10 +1,9 @@
-"use client";
 import React, { useState, useEffect } from 'react';
 import Header from '../header';
 import Footer from '../footer';
 import NameTag from '../nametag';
 import Modal from 'react-modal';
-import {affirmations} from '../state';
+import { affirmations } from '../state';
 
 function Home() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
