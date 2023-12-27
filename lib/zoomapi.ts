fb6b7b22adece4e49e7b0f1a576c3becbd918ee9
Ben Lickly <blickly@google.com>
2023-12-27 13:24:53 -0800

    Crete new file for wrapping the Zoom API
    This is used by creating a ZoomApiWrapper object, and then calling
    the Zoom API methods on that. Doing so enforces the correct ordering
    between configuration and making effectful API calls.

---
+++ lib/zoomapi.ts
@@ -0,0 +1,36 @@
+import zoomSdk, {ConfigOptions, ConfigResponse, GeneralMessageResponse }  from "@zoom/appssdk";
+
+export interface ZoomApiWrapper {
+  setVirtualForeground(imageData: ImageData): Promise<GeneralMessageResponse>;
+  removeVirtualForegroud(): Promise<GeneralMessageResponse>;
+}
+
+export function createFromConfig(options: ConfigOptions) {
+  return new ZoomApiImpl(options);
+}
+
+class ZoomApiImpl implements ZoomApiWrapper {
+  private configResponse: null|Promise<ConfigResponse> = null;
+  constructor(private configOptions: ConfigOptions) {}
+
+  initialize() {
+    if (this.configResponse == null) {
+      this.configResponse = zoomSdk.config(this.configOptions);
+    }
+    return this.configResponse;
+
+  }
+
+  async setVirtualForeground(imageData: ImageData): Promise<GeneralMessageResponse> {
+    const configResponse = await this.initialize();
+    console.log(`Zoom SDK config response: ${configResponse}`);
+    return zoomSdk.setVirtualForeground({imageData});
+  }
+
+  async removeVirtualForegroud(): Promise<GeneralMessageResponse> {
+    const configResponse = await this.initialize();
+    console.log(`Zoom SDK config response: ${configResponse}`);
+    return zoomSdk.removeVirtualForeground();
+  }
+
+}
