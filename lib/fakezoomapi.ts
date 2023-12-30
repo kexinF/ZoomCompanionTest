04cde092553705ccfa4b6f2067fe3cb30cbd1743
Ben Lickly <blickly@google.com>
2023-12-30 11:47:45 -0800

    Add fakezoomapi missing in last commit

---
+++ lib/fakezoomapi.ts
@@ -0,0 +1,11 @@
+import ZoomApiWrapper from './zoomapi';
+
+class FakeZoomApi implements ZoomApiWrapper {
+  async setVirtualForeground() {}
+  async removeVirtualForeground() {}
+}
+export {ZoomApiWrapper};
+
+export function createFromConfig() {
+  return new FakeZoomApi();
+}
