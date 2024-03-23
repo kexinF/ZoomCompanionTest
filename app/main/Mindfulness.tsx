7af4fc1474ec9006979066b29383c7ade2e64375
Kexin Feng <kexin@tamu.edu>
2024-03-22 22:23:34 -0500

    Move the style code in Mindfulness page to a separate css file (#34)
    * Move the style code in Mindfulness page to a separate css file
    
    * Update Mindfulness.tsx
    
    * Update Mindfulness.tsx
    
    * revise generateVideoPlayer function

--- app/main/Mindfulness.tsx
+++ app/main/Mindfulness.tsx
@@ -2,18 +2,19 @@
 
 "use client";
 
+import '../css/Mindfulness.css'; // Import CSS file
+
 interface VideoPlayerProps {
   videoId: string;
   Label: string;
 }
 
 const generateVideoPlayer = ({ videoId, Label }: VideoPlayerProps) => (
-  <div style={{ position: 'relative', width: '100%', height: 0, paddingBottom: '56.25%', marginBottom: '20px' }}>
+  <div className="video-wrapper">
     <iframe
       className='video'
       title='Youtube player'
       aria-label={Label}
-      style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
       src={`https://www.youtube.com/embed/${videoId}`}
     ></iframe>
   </div>
@@ -29,14 +30,14 @@ function Mindfullness() {
   const youtube3Label = 'youtube-video-3';
 
   return (
-      <div style={{ marginLeft: '20px', marginRight: '20px' }}>
-        <h2 style={{ fontSize: '30px', fontWeight: 'bold', display: 'inline-block' }}>Mindfulness</h2>
-          <div className="flex flex-col items-center mt-4">
-            {generateVideoPlayer({ videoId: youtube1ID, Label: youtube1Label })}
-            {generateVideoPlayer({ videoId: youtube2ID, Label: youtube2Label })}
-            {generateVideoPlayer({ videoId: youtube3ID, Label: youtube3Label })}
-          </div>
+    <div className="mindfulness-container">
+      <h2 className="title">Mindfulness</h2>
+      <div className="video-container">
+        {generateVideoPlayer({ videoId: youtube1ID, Label: youtube1Label })}
+        {generateVideoPlayer({ videoId: youtube2ID, Label: youtube2Label })}
+        {generateVideoPlayer({ videoId: youtube3ID, Label: youtube3Label })}
       </div>
+    </div>
   );
 }
 
